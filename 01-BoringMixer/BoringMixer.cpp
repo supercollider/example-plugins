690dcfdcf705befa7d3d36d1e9d4e0940f1f812c
Nathan Ho <nathan@snappizz.com>
2016-05-08 18:23:23 -0700

    more work on comments

--- 01-BoringMixer/BoringMixer.cpp
+++ 01-BoringMixer/BoringMixer.cpp
@@ -5,38 +5,38 @@ static InterfaceTable *ft;
 
 // declare struct to hold unit generator state
 struct BoringMixer : public Unit {
-    // this is blank because this particular ugen is stateless.
-    // in more advanced examples we'll have stuff here.
+    // in later examples, we will declare state variables here.
 };
 
-// in some older plugins these function declarations are wrapped in 'extern "C" { ... }'
+// older plugins wrap these function declarations in 'extern "C" { ... }'
 // no need to do that these days.
 static void BoringMixer_next(BoringMixer* unit, int inNumSamples);
 static void BoringMixer_Ctor(BoringMixer* unit);
 
 // the constructor function is called when a Synth containing this ugen is played.
-// it MUST be named PluginName_Ctor.
-// in later examples, this function will initialize state variables.
+// it MUST be named "PluginName_Ctor", and the argument must be "unit."
 void BoringMixer_Ctor(BoringMixer* unit) {
-    // set a calculation function.
-    // in later examples, we will have multiple calculation functions depending on input and output rates.
+    // in later examples, we will initialize state variables here.
+    
+    // set a calculation function. for now, we only have one calculation function.
     SETCALC(BoringMixer_next);
     // calculate one sample of output.
     // this allows plugging into other ugens with initial-rate inputs 
     BoringMixer_next(unit, 1);
 }
 
-// the calculation function can have any name, but this is conventional.
+// the calculation function can have any name, but this is conventional. the first argument must be "unit."
 // this function is called every control period (64 samples is typical)
+// Don't change the names of the arguments, or the helper macros won't work.
 void BoringMixer_next(BoringMixer* unit, int inNumSamples) {
 
     // IN and OUT are helper macros that return audio-rate input and output buffers.
-    // in SC source code, these buffers are often called "wire bufs" since they are how ugens communicate.
+    // In old ugens you'll see ZIN and ZOUT, which are not recommended.
     float *left = IN(0); // first input
     float *right = IN(1); // second input
     float *out = OUT(0); // first output
 
-    // loop through samples
+    // Loop through samples and do the computation.
     for (int i = 0; i < inNumSamples; i++) {
         out[i] = (left[i] + right[i]) * 0.5;
     }
